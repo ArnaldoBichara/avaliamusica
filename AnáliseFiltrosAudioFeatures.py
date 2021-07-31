f743d0c122a68575958f3c3794eda8bfae35a79e
ARNALDO ALMEIDA <arnaldo.bichara@gmail.com>
2021-07-31 17:05:05 -0300

    análise de AudioFeatures

--- AnáliseFiltrosAudioFeatures.py
+++ AnáliseFiltrosAudioFeatures.py
@@ -2,44 +2,42 @@
 import pandas as pd
 import numpy as np
 import pickle
-
-# 
+# seaborn é um ótimo visualizador sobre o matplotlib
+import seaborn as sea
+import matplotlib.pyplot as plt
+# %%
 dfAudioFeatures =  pd.read_pickle ("./FeatureStore/AudioFeatures.pickle")  
 dfUserAAudioFeatures =  pd.read_pickle ("./FeatureStore/UserA_AudioFeatures.pickle")  
 
 # %%
 dfAudioFeatures.tail(1)
 #%%
-speechiness = dfAudioFeatures[['speechiness']]
-speechiness.tail(1)
-
-#%% histograma
-a1 = dfAudioFeatures.plot.hist(by='speechiness', 
+#%% histograma de AudioFeatures
+dfAudioFeatures[['speechiness']].plot.hist(by='speechiness', 
                                bins=10, alpha=0.5)
+#%% histograma de AudioFeatures do UserA
+dfUserAAudioFeatures[['speechiness']].plot.hist(by='speechiness', 
+                               bins=10, alpha=0.5)
+#%% histograma de duration_ms AudioFeatures
+dfAudioFeatures[['duration_ms']].plot.hist(by='duration_ms', 
+                               bins=100, alpha=0.5)
+#%% normalizando duration_ms entre 0 e 1
+def normaliza_minmax(df):
+    return (df - df.min()) / ( df.max() - df.min())
 
-# %% ordenando o index. 
-dfUserAMusFeatures.info()
-df600kMus_Features.info()
+dfAudioFeatures[['duration_norm']] = normaliza_minmax(dfAudioFeatures[['duration_ms']])
+dfAudioFeatures.drop (columns=['duration_ms'], inplace=True)
+dfAudioFeatures[['duration_norm']].plot.hist(by='duration_norm', 
+                               bins=100, alpha=0.5)
+#%% correlação entre colunas
+columns = dfAudioFeatures.columns
+#%% tirando algumas colunas do cálculo de correlação
+dfMenosColunas = dfAudioFeatures.iloc[:,3:-1]
+matriz_de_correlacao = dfMenosColunas.corr()
+sea.heatmap(matriz_de_correlacao)
+plt.title("Correlação entre parâmetros de AudioFeatures")
+plt.show()
+#%% removendo coluna loudness
+dfAudioFeatures.drop(columns=['loudness'], inplace=True)
 #%%
-print (dfUserAMusFeatures.columns.values)
-print (df600kMus_Features.columns.values)
- 
-#%% buscando uma música específica da Enya nos dois datasets
-print(dfUserAMusFeatures[dfUserAMusFeatures['musica'].str.contains("anywhere is", na= False, case=False)][['id_musica','artista', 'musica']].to_string(index=False))
-print(df600kMus_Features[df600kMus_Features['musica'].str.contains("anywhere is", na= False, case=False)][['id_musica','artista', 'musica']].to_string(index=False))
-
-#%% verificando quantas músicas de User A existe em 600kMus
-listaMusEmComum = set(dfUserAMusFeatures['id_musica']).intersection(df600kMus_Features['id_musica'])
-#%%
-print (len(listaMusEmComum))
-print (len (dfUserAMusFeatures))
-
-#%%
-len(df600kMus_Features)
-# %% Incluindo músicas do user A no dataset de músicas & features
-dfAudioFeatures = pd.concat([df600kMus_Features, dfUserAMusFeatures], ignore_index=True, verify_integrity=True)
-
-# %%
-len(dfAudioFeatures)
-
-# %%
+dfAudioFeatures.describe()
