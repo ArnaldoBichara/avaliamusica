eeeeecb85e43fae79d516ea2a22fd5b7774c8a35
ARNALDO ALMEIDA <arnaldo.bichara@gmail.com>
2021-08-02 14:03:37 -0300

    Feito FiltraAudioFeature

--- AnáliseFiltrosAudioFeatures.py
+++ AnáliseFiltrosAudioFeatures.py
@@ -18,9 +18,17 @@ dfAudioFeatures[['speechiness']].plot.hist(by='speechiness',
 #%% histograma de AudioFeatures do UserA
 dfUserAAudioFeatures[['speechiness']].plot.hist(by='speechiness', 
                                bins=10, alpha=0.5)
+#%%
+# filtrando em AudioFeatures, apenas linhas
+# com speechiness < 0,6. Mais que isso certamente
+# não são músicas, são fala
+print (dfAudioFeatures.shape)
+dfAudioFeatures = dfAudioFeatures[dfAudioFeatures['speechiness'] < 0.6]
+print (dfAudioFeatures.shape)
 #%% histograma de duration_ms AudioFeatures
 dfAudioFeatures[['duration_ms']].plot.hist(by='duration_ms', 
                                bins=100, alpha=0.5)
+
 #%% normalizando duration_ms entre 0 e 1
 def normaliza_minmax(df):
     return (df - df.min()) / ( df.max() - df.min())
@@ -35,7 +43,19 @@ columns = dfAudioFeatures.columns
 dfMenosColunas = dfAudioFeatures.iloc[:,3:-1]
 matriz_de_correlacao = dfMenosColunas.corr()
 sea.heatmap(matriz_de_correlacao)
+ax = sea.heatmap(
+    matriz_de_correlacao, 
+    vmin=-1, vmax=1, center=0,
+    cmap=sea.diverging_palette(20, 220, n=200),
+    square=True
+)
+ax.set_xticklabels(
+    ax.get_xticklabels(),
+    rotation=45,
+    horizontalalignment='right'
+);
 plt.title("Correlação entre parâmetros de AudioFeatures")
+
 plt.show()
 #%% removendo coluna loudness
 dfAudioFeatures.drop(columns=['loudness'], inplace=True)
