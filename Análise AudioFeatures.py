a8254e7cb993e0f544ade4c841c1e4797ac35d0f
ArnaldoBichara <arnaldo.bichara@gmail.com>
2021-08-12 10:28:37 -0300

    análise audio features - pequena modificação

--- Análise AudioFeatures.py
+++ Análise AudioFeatures.py
@@ -21,42 +21,42 @@ plt.savefig("./Resultado das Análises/Histograma AudioFeatures UserA barra.pdf"
 # %% Para cada característica musical
 AnalisesTxt = open ('./Resultado das Análises/AudioFeatures.txt', 'w')
 
-print ('Danceability:', file=AnalisesTxt)
+print ('\nDanceability:', file=AnalisesTxt)
 print ("User A curte    : Media=","{:.3f}".format(dfUserAAudioFeatures['danceability'].mean())," Desvio Padrao=","{:.3f}".format(dfUserAAudioFeatures['danceability'].std()), file= AnalisesTxt )
 print ("User A nao curte: Media=","{:.3f}".format(dfUserAbarradoAudioFeatures['danceability'].mean())," Desvio Padrao=","{:.3f}".format(dfUserAbarradoAudioFeatures['danceability'].std()), file= AnalisesTxt )
 print ("Users           : Media=","{:.3f}".format(dfAudioFeatures['danceability'].mean())," Desvio Padrao=","{:.3f}".format(dfAudioFeatures['danceability'].std()), file= AnalisesTxt)
 
-print ('Energy:', file=AnalisesTxt)
+print ('\nEnergy:', file=AnalisesTxt)
 print ("User A curte    : Media=","{:.3f}".format(dfUserAAudioFeatures['energy'].mean())," Desvio Padrao=","{:.3f}".format(dfUserAAudioFeatures['energy'].std()), file= AnalisesTxt )
 print ("User A nao curte: Media=","{:.3f}".format(dfUserAbarradoAudioFeatures['energy'].mean())," Desvio Padrao=","{:.3f}".format(dfUserAbarradoAudioFeatures['energy'].std()), file= AnalisesTxt )
 print ("Users           : Media=","{:.3f}".format(dfAudioFeatures['energy'].mean())," Desvio Padrao=","{:.3f}".format(dfAudioFeatures['energy'].std()), file= AnalisesTxt)
 
-print ('Mode:', file=AnalisesTxt)
+print ('\nMode:', file=AnalisesTxt)
 print ("User A curte    : Media=","{:.3f}".format(dfUserAAudioFeatures['mode'].mean())," Desvio Padrao=","{:.3f}".format(dfUserAAudioFeatures['mode'].std()), file= AnalisesTxt )
 print ("User A nao curte: Media=","{:.3f}".format(dfUserAbarradoAudioFeatures['mode'].mean())," Desvio Padrao=","{:.3f}".format(dfUserAbarradoAudioFeatures['mode'].std()), file= AnalisesTxt )
 print ("Users           : Media=","{:.3f}".format(dfAudioFeatures['mode'].mean())," Desvio Padrao=","{:.3f}".format(dfAudioFeatures['mode'].std()), file= AnalisesTxt)
 
-print ('Speechiness:', file=AnalisesTxt)
+print ('\nSpeechiness:', file=AnalisesTxt)
 print ("User A curte    : Media=","{:.3f}".format(dfUserAAudioFeatures['speechiness'].mean())," Desvio Padrao=","{:.3f}".format(dfUserAAudioFeatures['speechiness'].std()), file= AnalisesTxt )
 print ("User A nao curte: Media=","{:.3f}".format(dfUserAbarradoAudioFeatures['speechiness'].mean())," Desvio Padrao=","{:.3f}".format(dfUserAbarradoAudioFeatures['speechiness'].std()), file= AnalisesTxt )
 print ("Users           : Media=","{:.3f}".format(dfAudioFeatures['speechiness'].mean())," Desvio Padrao=","{:.3f}".format(dfAudioFeatures['speechiness'].std()), file= AnalisesTxt)
 
-print ('Acousticness:', file=AnalisesTxt)
+print ('\nAcousticness:', file=AnalisesTxt)
 print ("User A curte    : Media=","{:.3f}".format(dfUserAAudioFeatures['acousticness'].mean())," Desvio Padrao=","{:.3f}".format(dfUserAAudioFeatures['acousticness'].std()), file= AnalisesTxt )
 print ("User A nao curte: Media=","{:.3f}".format(dfUserAbarradoAudioFeatures['acousticness'].mean())," Desvio Padrao=","{:.3f}".format(dfUserAbarradoAudioFeatures['acousticness'].std()), file= AnalisesTxt )
 print ("Users           : Media=","{:.3f}".format(dfAudioFeatures['acousticness'].mean())," Desvio Padrao=","{:.3f}".format(dfAudioFeatures['acousticness'].std()), file= AnalisesTxt )
 
-print ('Instrumentalness:', file=AnalisesTxt)
+print ('\nInstrumentalness:', file=AnalisesTxt)
 print ("User A curte    : Media=","{:.3f}".format(dfUserAAudioFeatures['instrumentalness'].mean())," Desvio Padrao=","{:.3f}".format(dfUserAAudioFeatures['instrumentalness'].std()), file= AnalisesTxt )
 print ("User A nao curte: Media=","{:.3f}".format(dfUserAbarradoAudioFeatures['instrumentalness'].mean())," Desvio Padrao=","{:.3f}".format(dfUserAbarradoAudioFeatures['instrumentalness'].std()), file= AnalisesTxt )
 print ("Users           : Media=","{:.3f}".format(dfAudioFeatures['instrumentalness'].mean())," Desvio Padrao=","{:.3f}".format(dfAudioFeatures['instrumentalness'].std()), file= AnalisesTxt )
 
-print ('Valence:', file=AnalisesTxt)
+print ('\nValence:', file=AnalisesTxt)
 print ("User A curte    : Media=","{:.3f}".format(dfUserAAudioFeatures['valence'].mean())," Desvio Padrao=","{:.3f}".format(dfUserAAudioFeatures['valence'].std()), file= AnalisesTxt )
 print ("User A nao curte: Media=","{:.3f}".format(dfUserAbarradoAudioFeatures['valence'].mean())," Desvio Padrao=","{:.3f}".format(dfUserAbarradoAudioFeatures['valence'].std()), file= AnalisesTxt )
 print ("Users           : Media=","{:.3f}".format(dfAudioFeatures['valence'].mean())," Desvio Padrao=","{:.3f}".format(dfAudioFeatures['valence'].std()), file= AnalisesTxt )
 
-print ('Liveness:', file=AnalisesTxt)
+print ('\nLiveness:', file=AnalisesTxt)
 print ("User A curte    : Media=","{:.3f}".format(dfUserAAudioFeatures['liveness'].mean())," Desvio Padrao=","{:.3f}".format(dfUserAAudioFeatures['liveness'].std()), file= AnalisesTxt )
 print ("User A nao curte: Media=","{:.3f}".format(dfUserAbarradoAudioFeatures['liveness'].mean())," Desvio Padrao=","{:.3f}".format(dfUserAbarradoAudioFeatures['liveness'].std()), file= AnalisesTxt )
 print ("Users           : Media=","{:.3f}".format(dfAudioFeatures['liveness'].mean())," Desvio Padrao=","{:.3f}".format(dfAudioFeatures['liveness'].std()), file= AnalisesTxt )
