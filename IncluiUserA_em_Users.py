a6180314b5928ced585d55915aaaaa476c42c9cd
ArnaldoBichara <arnaldo.bichara@gmail.com>
2021-08-14 11:05:15 -0300

    pequenas correções e mais logs

--- IncluiUserA_em_Users.py
+++ IncluiUserA_em_Users.py
@@ -25,6 +25,7 @@ df600kMusCurtem    =  pd.read_pickle ("./FeatureStore/MusUsers.pickle")
 dfUserA_MusCurte = dfUserA_MusCurte.drop(columns=['id_musica'])
 dfUserA_MusNaoCurte = dfUserA_MusNaoCurte.drop(columns='id_musica')
 #%%
+logging.info ('MusUsers antes = %s', df600kMusCurtem.shape)
 # %% Incluindo user A e user Abarrado no dataset User_MusCurtem
 df600kMusCurtem = pd.concat([df600kMusCurtem, dfUserA_MusCurte, dfUserA_MusNaoCurte], ignore_index=True, verify_integrity=True)        
 
@@ -34,7 +35,7 @@ df600kMusCurtem.drop_duplicates(inplace=True)
 # %%
 df600kMusCurtem.to_pickle('./FeatureStore/MusUsers.pickle')
 
-logging.info ('IncluUserA_em_Users shape = %s', df600kMusCurtem.shape)
+logging.info ('MusUser após concatenar com UserA = %s', df600kMusCurtem.shape)
 
 
 logging.info('IncluUserA_em_Users <<')
