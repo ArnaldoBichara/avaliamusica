65c1a17627088668b4e727ef2d2f50a40b515bf7
ArnaldoBichara <arnaldo.bichara@gmail.com>
2021-09-10 18:11:14 -0300

    treinamento Modelo Por Conteúdo

--- ClassifContentBased.py
+++ ClassifContentBased.py
@@ -23,31 +23,32 @@ logging.info('\n>> ConteudoTreina')
 #lendo dataset
 UserAFeatureSamples = pd.read_pickle ("./FeatureStore/UserAFeatureSamples.pickle")
 
-# função que executa o treinamento da classificação
-# e retorna predição dos dados de teste
-def ExecutaClassificação(dados_treino, labels_treino, dados_teste):
-    # njobs= -1 : usa todos os processadores da máquina
-    clf = RandomForestClassifier(njobs=-1)
-    clf.fit(dados_treino, labels_treino)
-    return clf.predict(dados_teste)
-
 #%% dividindo dataset em folds, para uso pelo procedimento de cross-validation
-num_folds = 5
+num_folds = 10
 
 X_folds = np.array_split ( UserAFeatureSamples.drop(columns=['classe']) , num_folds)
 y_folds = np.array_split ( np.array(UserAFeatureSamples['classe'])      , num_folds)
 
 #%% treinando o modelo
 acuracias=[]
+# n_jobs= -1 : usa todos os processadores da máquina
+modeloRF = RandomForestClassifier(n_jobs=-1)
 for i in range(num_folds):  
     dados_trein   = np.concatenate (X_folds[:i] + X_folds[i+1:])
     labels_trein = np.concatenate (y_folds[:i] + y_folds[i+1:])
     dados_teste   = X_folds[i]
     labels_teste = y_folds[i]
-    labels_predicao = ExecutaClassificação (dados_trein, labels_trein, dados_teste)
+    # executa treinamento
+    modeloRF.fit(dados_trein, labels_trein)
+    # faz predição dos dados de teste
+    labels_predicao = modeloRF.predict(dados_teste)
     acuracia = np.sum(labels_predicao == labels_teste)/len(labels_teste)
     acuracias.append(acuracia)
+print(acuracias)
+logging.info ("acuracias %s", acuracias)
 
-
+# salvando modelo em .pickle
+with open("./FeatureStore/modeloRandomForest.pickle", 'wb') as arq:
+    pickle.dump (modeloRF, arq)
 #%%
 logging.info('\n<< ConteudoTreina')
