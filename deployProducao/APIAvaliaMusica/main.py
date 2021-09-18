1397d6a81be00dfbb942efc0f4fdbb4cfe8fd6f8
ArnaldoBichara <arnaldo.bichara@gmail.com>
2021-09-18 18:58:01 -0300

    removida parte de autenticação

--- deployProducao/APIAvaliaMusica/main.py
+++ deployProducao/APIAvaliaMusica/main.py
@@ -1,26 +1,15 @@
-from flask import Flask, jsonify, request, Response
-from security import oauth, token_required
-import pickle
+from flask import Flask, jsonify, Response
 import pandas as pd
 from ClassifPredicao import Predicao
 
 app = Flask(__name__)
 
 @app.route('/predicao', methods=['POST'])
-@token_required
-def root(user:str) -> object:
+def root() -> object:
     try:
         return jsonify(Predicao())
     except:
         return Response("erro", status=404, mimetype='application/json')
-
-@app.route('/security', methods=['POST'])
-def security() -> object:
-    data = request.authorization
-    if not data or 'username' not in data or 'password' not in data:
-        return Response("erro", status=404, mimetype='application/json')
-    else:
-        return oauth(data['username'])
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
