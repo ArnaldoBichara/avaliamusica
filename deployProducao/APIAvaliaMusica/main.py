f9bc0e047ec816d4738f79edb347f3aec4553926
ArnaldoBichara <arnaldo.bichara@gmail.com>
2021-09-29 13:33:20 -0300

    incluído CORS no flask

--- deployProducao/APIAvaliaMusica/main.py
+++ deployProducao/APIAvaliaMusica/main.py
@@ -1,10 +1,14 @@
 from flask import Flask, jsonify, Response
+from flask_cors import CORS, cross_origin
 import pandas as pd
 from ClassifPredicao import Predicao
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/predicao', methods=['GET'])
+@cross_origin()
 def root() -> object:
     try:
         return jsonify(Predicao())
